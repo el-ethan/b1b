f47132fab934f3197ff47b61a312731ec3568ec3
el-ethan <elethan@gmail.com>
2015-02-25 14:20:47 -0600

    Change width and make layout more vertical

--- b1b.py
+++ b1b.py
@@ -1,3 +1,4 @@
+# TODO: max of 4 characters can be entered, buttons on top
 from tkinter import *
 import tkinter.font
 import math
@@ -23,7 +24,7 @@ class Application(Frame):
     def create_widgets(self):
         left_frame = Frame(self)
         self.char_entry = Entry(left_frame, text='Type characters here')
-        left_frame.pack(side=LEFT, fill=BOTH, expand=True)
+        left_frame.pack(side=TOP, fill=BOTH, expand=True)
         self.char_entry.pack(side=TOP, fill=BOTH, expand=True)
 
         var = IntVar()
@@ -68,36 +69,22 @@ class Application(Frame):
         self.display_frame = LabelFrame(self)
         self.display_frame.pack(side=RIGHT, fill=BOTH, expand=True)
 
-        height = 10
-        width =  math.ceil(len(self.SC_fonts) / 10)    # Need to round len up
+        height = math.ceil(len(self.TCSC_fonts) / 5)
+        width = 5
         fonts = self.font_gen()
-        try:
-            for r in range(height):
-                for c in range(width):
+        # try:
+        for r in range(height):
+            for c in range(width):
 
-                        Label(self.display_frame,
-                              text=chars, relief=RIDGE,
-                              font=(next(fonts), 50)).grid(row=r, column=c, sticky=N+E+S+W)
-        except StopIteration:
-            pass
+                    Label(self.display_frame,
+                          text=chars, relief=RIDGE,
+                          font=(next(fonts), 55)).grid(row=r, column=c, sticky=N+E+S+W)
+        # except StopIteration:
+        #     pass
 
     def font_gen(self):
-
-        all_fonts = tkinter.font.families()
-        self.TC_fonts = []
-        self.SC_fonts = []
-        for font in all_fonts:
-            if 'SC' in font:
-                self.SC_fonts.append(font)
-            elif ' TC' in font:
-                self.TC_fonts.append(font)
-        self.TCSC_fonts = self.SC_fonts + self.TC_fonts
-        for font in self.SC_fonts:
+        for font in self.TCSC_fonts:
             yield font
-
-
-
-
 
 root = Tk()
 root.resizable(0, 0)
