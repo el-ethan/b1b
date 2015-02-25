ad64ee7b4f2129fb06be4bc48220edfbf9492d07
el-ethan <elethan@gmail.com>
2015-02-25 15:13:33 -0600

    Fixed layout; fewer characters don't leave space.

--- b1b.py
+++ b1b.py
@@ -10,34 +10,33 @@ class Application(Frame):
         self.pack(fill=BOTH, expand=True)
 
         all_fonts = tkinter.font.families()
-        self.TC_fonts = []
-        self.SC_fonts = []
+        self.TC_fonts = []  # 9 total on my Mac
+        self.SC_fonts = []  # 15 total on my Mac
         for font in all_fonts:
             if 'SC' in font:
                 self.SC_fonts.append(font)
             elif ' TC' in font:
                 self.TC_fonts.append(font)
         self.TCSC_fonts = self.SC_fonts + self.TC_fonts
-
         self.create_widgets()
 
     def create_widgets(self):
         top_frame = Frame(self)
-        self.char_entry = Entry(top_frame)
-        self.char_entry.insert('0', '比一笔')
         top_frame.pack(side=TOP, fill=BOTH, expand=True)
-        self.char_entry.pack(side=TOP, fill=BOTH, expand=True)
 
+
+        top_right_frame = Frame(top_frame, padx=10)
+        top_right_frame.pack(side=RIGHT)
         var = IntVar()
-        r1 = Radiobutton(top_frame,
+        r1 = Radiobutton(top_right_frame,
                          variable=var,
                          value=1,
                          text='Traditional')
-        r2 = Radiobutton(top_frame,
+        r2 = Radiobutton(top_right_frame,
                          variable=var,
                          value=2,
                          text='Simplified')
-        r3 = Radiobutton(top_frame,
+        r3 = Radiobutton(top_right_frame,
                          variable=var,
                          value=3,
                          text='TC & SC')
@@ -45,15 +44,21 @@ class Application(Frame):
         r2.pack(side=TOP, fill=BOTH, expand=True)
         r3.pack(side=TOP, fill=BOTH, expand=True)
 
-        button_frame = Frame(top_frame)
-        show_b = Button(button_frame, text='Show', command=self.set_fonts)
-        quit_b = Button(button_frame, text='Quit', command=self.quit)
-        button_frame.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+        top_left_frame = Frame(top_frame, padx=10)
+        self.char_entry = Entry(top_left_frame)
+        self.char_entry.insert('0', '比一笔')
+        self.char_entry.pack(side=TOP, fill=BOTH, expand=True)
+        show_b = Button(top_left_frame, text='Show', command=self.set_fonts)
+        quit_b = Button(top_left_frame, text='Quit', command=self.quit)
+
+        top_left_frame.pack(side=LEFT, fill=BOTH, expand=True)
         show_b.pack(side=LEFT, fill=BOTH, expand=True)
         quit_b.pack(side=LEFT, fill=BOTH, expand=True)
 
         self.display_frame = LabelFrame(self)
-        self.display_frame.pack(side=RIGHT, fill=BOTH, expand=True)
+        self.display_frame.pack(side=TOP, fill=BOTH, expand=True)
+        # Set initial display layout
         self.set_fonts()
         # height = math.ceil(len(self.TCSC_fonts) / 5)
         # width = 5
@@ -71,10 +76,10 @@ class Application(Frame):
         chars = self.char_entry.get()
         self.display_frame.destroy()
         self.display_frame = LabelFrame(self)
-        self.display_frame.pack(side=RIGHT, fill=BOTH, expand=True)
+        self.display_frame.pack(side=TOP, fill=BOTH, expand=True)
 
-        height = math.ceil(len(self.TCSC_fonts) / 5)
-        width = 5
+        height = math.ceil(len(self.TCSC_fonts) / 4)
+        width = 4
         fonts = self.font_gen()
         try:
             for r in range(height):
