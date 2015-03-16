077b9f0a34e5b342cd6e0f2e7c3d41973857e755
el-ethan <elethan@gmail.com>
2015-03-15 23:34:32 -0500

    Improve font_find functionality
    font_find now has clickable listboxes with font
    names. Clicking on a font name changes to display
    to that font.

--- font_find.py
+++ font_find.py
@@ -14,13 +14,36 @@ class Application(Frame):
         self.build_test_area()
 
     def build_fontlists(self):
-        font_disp1 = Text(self, width=30)
-        font_disp1.pack(side=LEFT)
-        for font in self.all_fonts:
-            font_disp1.insert('0.0', font + '\n')
+        left_frame1 = LabelFrame(self)
+        left_frame1.pack(side=LEFT, fill=BOTH)
 
-        font_disp2 = Text(self, width=30)
-        font_disp2.pack(side=LEFT)
+        Label(left_frame1,
+              text='All Fonts',
+              font=('Avenir Next', 20),
+              bg='cornflower blue',
+              bd=10).pack(side=TOP, fill=BOTH)
+        self.font_list1 = Listbox(left_frame1,
+                                  width=30,
+                                  height=20,
+                                  font=('Avenir Next', 12))
+        self.font_list1.pack(side=TOP, fill=BOTH)
+        for font in self.all_fonts:
+            self.font_list1.insert(END, font)
+            self.font_list1.bind('<Double-Button-1>', self.change_font1)
+
+        left_frame2 = LabelFrame(self)
+        left_frame2.pack(side=LEFT, fill=BOTH)
+
+        Label(left_frame2,
+              text='Potential Chinese Fonts',
+              font=('Avenir Next', 20),
+              bg='cornflower blue',
+              bd=10).pack(side=TOP, fill=BOTH)
+        self.font_list2 = Listbox(left_frame2,
+                                  width=30,
+                                  height=20,
+                                  font=('Avenir Next', 12))
+        self.font_list2.pack(side=TOP, fill=BOTH)
 
         for font in self.all_fonts:
             if any((('ti ' in font),
@@ -30,25 +53,29 @@ class Application(Frame):
                    ('gb' in font),
                    ('GB' in font),
                    (font.endswith('ti')))):
-                font_disp2.insert('0.0', font + '\n')
+                self.font_list2.insert(END, font)
+                self.font_list2.bind('<Double-Button-1>', self.change_font2)
 
     def build_test_area(self):
-        self.test_entry = Entry(self)
-        self.test_entry.pack(side=TOP)
+        test_frame = LabelFrame(self)
+        test_frame.pack(side=LEFT, fill=BOTH)
 
-        self.test_display = Label(self,
-                             text='繁體字\n\n简体字',
-                             font=('', 55),
-                             padx=30,
-                             pady=60)
-        self.test_display.pack(side=TOP)
-        change_button = Button(self,
-                               command=self.change_font,
-                               text='Change')
-        change_button.pack(side=TOP)
+        self.test_display = Label(test_frame,
+                             text='繁體字\n简体字',
+                             font=('', 60),
+                             padx=80)
+        self.test_display.pack(side=RIGHT, fill=BOTH, expand=True)
+        # change_button = Button(self,
+        #                        command=self.change_font,
+        #                        text='Change')
+        # change_button.pack(side=TOP)
 
-    def change_font(self):
-        font = self.test_entry.get()
+    def change_font1(self, event):
+        font = self.font_list1.get(ACTIVE)
+        self.test_display.config(font=(font, 55))
+
+    def change_font2(self, event):
+        font = self.font_list2.get(ACTIVE)
         self.test_display.config(font=(font, 55))
 
 
